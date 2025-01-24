ebf0c7b64990f3bf9a34063ceb2b2dc99124f2b6
SK sanoyar ali <asksanoyar33@gmail.com>
2025-01-24 15:24:20 +0530

    24_01_25 solution uploadded

---
+++ leetcode/24_01_2025_safe_states.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+       int n=graph.size();
+       vector<int> revadj[n];
+       for(int i=0;i<n;i++){
+        for(int j=0;j<graph[i].size();j++){
+            revadj[graph[i][j]].push_back(i);
+        }
+       }
+       vector<int> indegree(n,0);
+       for(int i=0;i<n;i++){
+        for(auto it:revadj[i]){
+            indegree[it]++;
+        }
+       }
+       queue<int> q;
+       for(int i=0;i<n;i++){
+        if(indegree[i]==0){
+            q.push(i);
+        }
+       }
+       vector<int> safe;
+       while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        safe.push_back(node);
+        for(auto it:revadj[node]){
+            indegree[it]--;
+            if(indegree[it]==0){
+                q.push(it);
+            }
+        }
+       }
+       sort(safe.begin(),safe.end());
+       return safe;
+    }
+};
