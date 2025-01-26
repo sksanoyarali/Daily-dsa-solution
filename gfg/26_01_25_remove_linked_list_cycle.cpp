70b0ea0965e145da1304555157b1122921b600a0
SK sanoyar ali <asksanoyar33@gmail.com>
2025-01-26 18:38:25 +0530

    26_01_25 solution uploaded

---
+++ gfg/26_01_25_remove_linked_list_cycle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    // Function to remove a loop in the linked list.
+    void removeLoop(Node* head) {
+        // code here
+        if(head==NULL) return;
+        unordered_set<Node*> st;
+        Node* temp=head;
+        while(temp->next!=NULL){
+            if(st.find(temp->next)!=st.end()){
+                temp->next=NULL;
+                return;
+            }
+            st.insert(temp);
+            temp=temp->next;
+        }
+    }
+};
